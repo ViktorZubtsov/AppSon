fb4ee0fb79fe8c0521b7e1658f3f609e9dbd1144
mr.viktor.1996 <mr.viktor.1996@yandex.ru>
2020-07-19 11:16:22 +0700

    Stylized the buttons, made them adaptive, and wrote the Scroll class.

---
+++ src/js/scroll.js
@@ -0,0 +1,26 @@
+class Scroll  {
+    constructor (el) {
+        this.el = el;
+        
+    }
+    
+    goUp() {
+        let timeOut;
+        let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+        if(top > 0) {
+        window.scrollBy(0,-100);
+        timeOut = setTimeout(this.goUp(),20);
+        } else {
+            clearTimeout(timeOut);
+        }
+    }
+    goDawn(){
+        window.scrollBy(0, window.innerHeight);
+
+    }
+
+}
+
+let scrollToTop = new Scroll(document.querySelector('#scrollToTop'));
+let scrollToDawn = new Scroll(document.querySelector('#scrollToDawn'));
+
