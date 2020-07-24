974f66af0fc266f44c7a7f4e95ee7bc13b2c017b
mr.viktor.1996 <mr.viktor.1996@yandex.ru>
2020-07-25 06:43:16 +0700

    hotfix nav

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,19 @@
 (function () {
     let menuIcon =  document.querySelector('.menu-icon'),
     nav = document.querySelector('.navigation'),
-    navMenuMobile = document.querySelector('.nav-menu_mobile');
+    navMenuMobile = document.querySelector('.nav-menu_mobile'),
+    body = document.querySelector('.body');
+   
 
     return menuIcon.onclick = function(){
         menuIcon.classList.toggle('menu-icon_active');
         nav.classList.toggle('navigation_active');
         navMenuMobile.classList.toggle('nav-menu_mobile_active');
+        body.classList.toggle("hidden");
+ 
     };
-}());
+    
+
+})();
 
 
